5d5852a0690f523dcf792aafc89249395fca3192
Yann Druffin <yann.druffin@gmail.com>
2018-04-05 19:51:35 -0700

    Added Csv output

--- main.go
+++ main.go
@@ -1,28 +1,77 @@
 package main
 
 import (
+	"encoding/csv"
+	"flag"
 	"fmt"
+	"io"
 	"log"
+	"os"
 
+	"github.com/yanndr/topstories/client"
 	"github.com/yanndr/topstories/client/hackernews"
 )
 
 func main() {
-	c := hackernews.New(20)
+	csvPtr := flag.Bool("csv", false, "Save the result to a csv file.")
+	path := flag.String("o", "outupt.csv", "output file name")
+	n := flag.Int("n", 20, "number of stories to display")
+	c := flag.Int("c", 20, "max concurency allowed")
+	flag.Parse()
 
-	resp, err := c.Get(20)
+	cl := hackernews.New(*c)
+	resp, err := cl.Get(*n)
 
+	if err != nil {
+		log.Panicf("cannot get the stories: %s", err)
+	}
+
+	if *csvPtr {
+		f, err := os.Create(*path)
+		if err != nil {
+			log.Panicf("cannot create the file: %s", err)
+		}
+		defer f.Close()
+		err = outputToCsv(f, resp)
+		if err != nil {
+			log.Panicf("cannot save to csv: %s", err)
+		}
+		return
+	}
+
+	err = handleResponse(resp, func(s client.Story) error {
+		_, err := fmt.Printf("|%-60s|%-100s|\n", s.Title(), s.URL())
+		return err
+	})
 	if err != nil {
 		log.Panic(err)
 	}
+}
 
+func handleResponse(resp <-chan client.Response, f func(client.Story) error) error {
 	for r := range resp {
 		if r.Error != nil {
-			log.Panic(err)
+			return r.Error
 		}
-		fmt.Printf("|%-55s|%-110s|\n", r.Story.Title(), r.Story.URL())
-		if r.Error != nil {
-			log.Panic(err)
+		err := f(r.Story)
+		if err != nil {
+			return err
 		}
 	}
+	return nil
+}
+
+func outputToCsv(f io.Writer, resp <-chan client.Response) error {
+
+	w := csv.NewWriter(f)
+	handleResponse(resp, func(s client.Story) error {
+		return w.Write([]string{s.Title(), s.URL()})
+	})
+	w.Flush()
+
+	if err := w.Error(); err != nil {
+		return fmt.Errorf("cannot write the csv file: %s", err)
+	}
+
+	return nil
 }
--- main_test.go
+++ main_test.go
@@ -1,11 +1,58 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"testing"
+
+	"github.com/yanndr/topstories/client"
 )
 
-func TestMain(t *testing.T) {
-	main()
-	log.Println("Nothing to test yet.")
+type fakeStory struct {
+}
+
+func (fakeStory) Title() string {
+	return "title"
+}
+
+func (fakeStory) URL() string {
+	return "url"
+}
+func TestHandleResponse(t *testing.T) {
+
+	tt := []struct {
+		name    string
+		story   fakeStory
+		errItem error
+		errFunc error
+	}{
+		{name: "Response with story", story: fakeStory{}},
+		{name: "Response with error", errItem: fmt.Errorf("error"), errFunc: nil},
+		{name: "Response with func error", errFunc: fmt.Errorf("error")},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			c := make(chan client.Response)
+			go func() {
+				defer close(c)
+				c <- client.Response{Story: tc.story, Error: tc.errItem}
+			}()
+
+			called := false
+			err := handleResponse(c, func(s client.Story) error {
+				called = true
+				return tc.errFunc
+			})
+
+			if err != tc.errFunc && err != tc.errItem {
+				t.Fatalf("Expected %v got %v", tc.errFunc, err)
+			}
+
+			if tc.errItem != nil && called {
+				t.Fatal("Function not expected to be call")
+			} else if tc.errItem == nil && !called {
+				t.Fatal("Function  expected to be call")
+			}
+		})
+	}
 }
