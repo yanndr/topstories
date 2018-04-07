913124047e99f87e17d35c9ab66c7aac6cc61299
Yann Druffin <yann.druffin@gmail.com>
2018-04-06 17:10:55 -0700

    fixing main test

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"log"
+	"io"
 	"testing"
 
 	"github.com/yanndr/topstories/provider"
@@ -19,47 +19,35 @@ func (fakeStory) Title() string {
 func (fakeStory) URL() string {
 	return "url"
 }
-func TestHandleResponse(t *testing.T) {
 
-	tt := []struct {
-		name    string
-		story   fakeStory
-		errItem error
-		errFunc error
-	}{
-		{name: "Response with story", story: fakeStory{}},
-		{name: "Response with error", errItem: fmt.Errorf("error"), errFunc: nil},
-		{name: "Response with func error", errFunc: fmt.Errorf("error")},
-	}
-
-	for _, tc := range tt {
-		t.Run(tc.name, func(t *testing.T) {
-			c := make(chan provider.Response)
-			go func() {
-				defer close(c)
-				c <- provider.Response{Story: tc.story, Error: tc.errItem}
-			}()
-
-			called := false
-			err := handleResponse(c, func(s provider.Story) error {
-				called = true
-				return tc.errFunc
-			})
-
-			if err != tc.errFunc && err != tc.errItem {
-				t.Fatalf("Expected %v got %v", tc.errFunc, err)
-			}
-
-			if tc.errItem != nil && called {
-				t.Fatal("Function not expected to be call")
-			} else if tc.errItem == nil && !called {
-				t.Fatal("Function  expected to be call")
-			}
-		})
-	}
+type fakeProvider struct {
 }
 
-func TestOutputToCsv(t *testing.T) {
+func (fakeProvider) GetStories(limit int) (<-chan provider.Response, error) {
+	ch := make(chan provider.Response)
+
+	go func() {
+		for i := 0; i < limit; i++ {
+			ch <- provider.Response{Story: &fakeStory{}}
+		}
+		close(ch)
+	}()
+	return ch, nil
+}
+
+type fakeStoryWriter struct {
+	w io.Writer
+}
+
+func (w fakeStoryWriter) Write(s provider.Story) error {
+	_, err := w.w.Write([]byte(fmt.Sprintf("|%-60s|%-100s|\n", s.Title(), s.URL())))
+	return err
+}
+
+func (fakeStoryWriter) Flush() error { return nil }
+
+func TestRun(t *testing.T) {
+
 	tt := []struct {
 		name    string
 		story   fakeStory
@@ -73,17 +61,8 @@ func TestOutputToCsv(t *testing.T) {
 
 	for _, tc := range tt {
 		t.Run(tc.name, func(t *testing.T) {
-			c := make(chan provider.Response)
-			go func() {
-				defer close(c)
-				c <- provider.Response{Story: tc.story, Error: tc.errItem}
-			}()
-
-			buf := bytes.NewBufferString("")
-			outputToCsv(buf, c)
-			if len(buf.String()) == 0 && tc.errItem == nil && tc.errFunc == nil {
-				log.Fatalf("Expected value got no value on the buffer.")
-			}
+			b := &bytes.Buffer{}
+			run(&fakeProvider{}, &fakeStoryWriter{w: b}, 10)
 		})
 	}
 }
